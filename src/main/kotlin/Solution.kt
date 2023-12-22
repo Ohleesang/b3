b748153a5073920b48b5aeb1ffb28d616c65bccc
Ohleesang <jammmmed@gmail.com>
2023-12-22 09:35:30 +0900

    sortedBy 혹은 sortedWith를 이용

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -8,17 +8,36 @@
 
 class Solution {
     fun solution(N: Int, stages: IntArray): IntArray {
-        var answer = IntArray(N,{0})
+        var answer = intArrayOf()
+        var result = mutableMapOf<Int, Double>()//<stage,실패율>
         var stageList = stages.toMutableList()
         stageList.sort()
-        for(stage in 1..N){
+
+        for (stage in 1..N) {
+            //초기화
+            var count = 0
+            var size = stageList.size
+            //실폐율 계산
+            stageList.forEach {
+                if (stage >= it) count++
+            }
+            //stage,실폐율 입력
+            result.put(stage, (count.toDouble() / size))
+            //list 사이즈 줄이기
+            for (i in 0..count - 1) {
+                stageList.removeFirst()
+            }
 
         }
+        //정렬기능을 쓰기위해 list로 치환이후 다시 map으로 변환후, 키값만 출력
+        var sortedMap = result.toList().sortedWith(compareBy { -it.second }).toMap()
+        answer = sortedMap.keys.toIntArray()
         return answer
     }
 }
-fun main(){
+
+fun main() {
     var a = Solution()
-    a.solution(5, intArrayOf(2,1,2,6,2,4,3,3))//3,4,2,1,5
-    a.solution(4, intArrayOf(4,4,4,4,4))//4,1,2,3
+    a.solution(5, intArrayOf(2, 1, 2, 6, 2, 4, 3, 3))//3,4,2,1,5
+    a.solution(4, intArrayOf(4, 4, 4, 4, 4))//4,1,2,3
 }
